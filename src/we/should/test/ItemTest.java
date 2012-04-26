fc0dc129196f0b859d6a5f8e45c2a098ab2cba5a
Davis Shepherd <dgshep@gmail.com>
2012-04-26 10:41:16 -0700

    Added Item Test

---
+++ src/we/should/test/ItemTest.java
@@ -0,0 +1,21 @@
+package we.should.test;
+
+import java.util.*;
+
+import we.should.WeShouldActivity;
+import we.should.list.*;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class ItemTest extends
+		ActivityInstrumentationTestCase2<WeShouldActivity> {
+	Category C;
+
+	public ItemTest() {
+		super("we.should.WeShouldActivity", WeShouldActivity.class);
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	protected void setUp(){
+		
+	}
+}
