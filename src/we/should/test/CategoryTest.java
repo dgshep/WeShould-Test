cad7b1c3c9be052c4b0fc4f73c192537f3e89af7
Davis Shepherd <dgshep@gmail.com>
2012-05-11 20:42:22 -0700

    updated Category constructor test

--- src/we/should/test/CategoryTest.java
+++ src/we/should/test/CategoryTest.java
@@ -1,9 +1,11 @@
 package we.should.test;
 
 import java.util.LinkedList;
+import java.util.List;
 
 import we.should.list.Category;
 import we.should.list.Field;
+import we.should.list.FieldType;
 import we.should.list.GenericCategory;
 import junit.framework.TestCase;
 
@@ -33,14 +35,15 @@ public class CategoryTest extends TestCase {
 	public void testConstructor(){
 		Category c = new GenericCategory("Test", new LinkedList<Field>(), null);
 		assertEquals("Test", c.getName());
-		c = new GenericCategory(null, null, null);
-		assertEquals("", c.getName());
 	}
 	public void testEquals(){
 		Category c1 = new GenericCategory("c1", Field.getDefaultFields(), null);
 		Category c2 = new GenericCategory("c1", Field.getDefaultFields(), null);
 		Category c3 = new GenericCategory("c2", Field.getDefaultFields(), null);
-		Category c4 = new GenericCategory("c1", Field.getMovieFields(), null);
+		Field f = new Field("new", FieldType.MultilineTextField);
+		List<Field> newFields = new LinkedList<Field>();
+		newFields.add(f);
+		Category c4 = new GenericCategory("c1", newFields, null);
 		assertEquals(c1, c2);
 		assertEquals(c1, c1);
 		assertFalse(c1.equals(c3));
